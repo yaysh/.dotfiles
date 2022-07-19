42572cb2e72606bd0034330fdd01164ef608e556
Jens <jens.madsen93@gmail.com>
2022-07-19 21:52:58 +0200

    Update

--- nvim/lua/settings/keymaps.lua
+++ nvim/lua/settings/keymaps.lua
@@ -6,14 +6,15 @@ local function nmap(shortcut, command)
  map("n", shortcut, command)
 end
 
- -- Telescope bindings
+-- Telescope bindings
 nmap("<leader>ff", "<cmd>Telescope find_files<CR>")
 nmap("<leader>fg", "<cmd>Telescope live_grep<CR>")
 nmap("<leader>fb", " <cmd>Telescope buffers<CR>")
 nmap("<leader>fh", "<cmd>Telescope help_tags<CR>")
 nmap("<leader><leader>w", "<Plug>(easymotion-bd-w)")
 nmap("<leader>L", "<Plug>(easymotion-overwin-line)")
---  -- Scala metals 
+
+-- Scala metals 
 nmap("<silent>gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
 nmap("<silent>K", "<cmd>lua vim.lsp.buf.hover()<CR>")
 nmap("<silent>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
@@ -24,12 +25,12 @@ nmap("<silent><leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
 nmap("<silent><leader>fo", "<cmd>lua vim.lsp.buf.formatting()<CR>")
 nmap("<silent><leader>k", "<cmd>lua vim.lsp.buf.code_action()<CR>")
 nmap("<silent><leader>ws", '<cmd>lua require"metals".worksheet_hover()<CR>')
- -- nmap(" <silent><leader>a <cmd>lua require"metals".open_all_diagnostics()<CR>
 nmap("<silent><leader>a", "<cmd>lua vim.diagnostic.setqflist()<CR>")
 nmap("<silent>[c", "<cmd>lua vim.lsp.diagnostic.goto_prev { wrap = false }<CR>")
 nmap("<silent>]c", "<cmd>lua vim.lsp.diagnostic.goto_next { wrap = false }<CR>")
 nmap("<silent><leader>mm", '<cmd>lua require"telescope".extensions.metals.commands()<CR>')
---  -- Mapping for dap (debugging)
+
+-- Mapping for dap (debugging)
 nmap("<silent><leader>dt", '<cmd>lua require"dap".toggle_breakpoint()<CR>')
 nmap("<silent><leader>dc", '<Cmd>lua require"dap".continue()<CR>')
 nmap("<silent><leader>dso", '<Cmd>lua require"dap".step_over()<CR>')
@@ -40,12 +41,11 @@ nmap("<silent><leader>dl", '<Cmd>lua require"dap".run_last()<CR>')
 nmap("<silent><leader>dK", '<Cmd>lua require"dap.ui.widgets".hover()<CR>')
 nmap("<silent><leader>dC", '<Cmd>lua require"dap".terminate()<CR>')
 nmap("<silent><leader>dui", '<cmd>lua require"dapui".toggle()<CR>')
---  -- Nerdtree
+
+-- NvimTree
 nmap("<leader>n", ":NvimTreeFocus<CR>")
 nmap("<C-t>", ':NvimTreeToggle<CR>')
 
--- Bufferlines
--- nmap("<C-l>", ":BufferLineCycleNext<CR>")
--- nmap("<C-h>", ":BufferLineCyclePrev<CR>")
+-- Bufferliens
 nmap("L", ":BufferLineCycleNext<CR>")
 nmap("H", ":BufferLineCyclePrev<CR>")
