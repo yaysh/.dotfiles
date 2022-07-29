d8940952549f521124889504eccf9791ac3b0904
Jens <jens.madsen93@gmail.com>
2022-07-29 13:14:28 +0200

    Fixed play /pause

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo apt-get install wget
 sudo apt install lm-sensors
 sudo apt-get install fonts-powerline
 sudo apt-get install i3
+sudo apt-get install playerctl
 sudo apt-get install polybar
 sudo sh ./install-scripts/font.sh
 sudo sh ./after_git.sh
