ab261e025e4602945a0189a867ef2528cb61a031
Jens <jens.madsen93@gmail.com>
2022-07-29 02:52:23 +0200

    Hm 2

--- symlink.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.config/nvim
-mv -f .bashrc ~/.bashrc
-mv -f init.vim ~/.config/nvim/init.vim
-mv -f .ideavimrc ~/.ideavimrc
-
-ln ~/.bashrc .bashrc
-ln ~/.config/nvim/init.vim init.vim
-ln ~/.ideavimrc .ideavimrc
-ln ~/.config/nvim/* ./nvim
