2ddc5355a9fcb613ae1ae859c2f345a34e4b3eed
Jens <jens.madsen93@gmail.com>
2022-07-30 02:07:02 +0200

    Hm

--- nvim/lua/settings/keymaps.lua
+++ nvim/lua/settings/keymaps.lua
@@ -17,10 +17,6 @@ inoremap('"', '""<Esc>ha')
 inoremap("'", "\'\'<Esc>ha")
 inoremap('`', '``<Esc>ha')
 
--- Snippet
--- nmap("<TAB>", "<cmd>lua require('luasnip').jump(1)<Cr>")
--- nmap("<S-TAB>", "<cmd>lua require('luasnip').jump(-1)<Cr>")
-
 -- Telescope bindings
 nmap("<leader>ff", "<cmd>Telescope find_files<CR>")
 nmap("<leader>fg", "<cmd>Telescope live_grep<CR>")
