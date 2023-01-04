6f5397d22e1a7365784bd84ccb2cac34b32ee303
yaysh <jens.madsen93@gmail.com>
2023-01-04 11:45:07 +0100

    Updated install file

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
-sudo apt-get install ripgrep
-sudo apt-get install wget
-sudo apt install lm-sensors
-sudo apt-get install fonts-powerline
-sudo apt-get install i3
-sudo apt-get install playerctl
-sudo apt-get install polybar
-sudo apt-get install tmux
-sudo apt-get install rofi
-sudo apt-get install flameshot
-sudo apt-get install curl
-sudo apt-get install fuse
-sudo apt-get install picom
-sudo apt-get install xclip
-sudo apt-get install feh
-
-sudo sh ./install-scripts/font.sh
+sudo apt-get install ripgrep -y
+sudo apt-get install wget -y
+sudo apt install lm-sensors -y
+sudo apt-get install fonts-powerline -y
+sudo apt-get install i3 -y
+sudo apt-get install playerctl -y
+sudo apt-get install polybar -y
+sudo apt-get install tmux -y
+sudo apt-get install rofi -y
+sudo apt-get install flameshot -y
+sudo apt-get install curl -y
+sudo apt-get install fuse -y
+sudo apt-get install picom -y
+sudo apt-get install xclip -y
+sudo apt-get install feh -y
+sudo apt-get install flameshot -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
+unzip JetBrainsMono.zip -d ~/.fonts
+fc-cache -fv
 sudo sh ./after_git.sh
 
 
